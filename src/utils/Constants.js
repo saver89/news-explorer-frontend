58d123d16f2f4de8acfbfa9305578df6df6c73e4
user.email <neelkim1989@gmail.com>
2020-12-10 01:26:29 +0500

    изменить адрес апи

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,5 @@
 const newsApiKey = '4c931c21a3b7439ebddddd28c3b3e26a',
   newsApiUrl = 'https://nomoreparties.co/news/v2/everything',
-  mainApiUrl = 'https://api.neelkim.students.nomoreparties.co';
+  mainApiUrl = 'https://api.nee.students.nomoreparties.co';
 
 export { newsApiKey, newsApiUrl, mainApiUrl };
